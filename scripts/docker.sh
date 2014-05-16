54e12d248e0577ad6e4c989a1ed2f901f850aae4
Alistair Israel <aisrael@gmail.com>
2014-05-16 10:55:01 +0800

    Revert to lxc-docker (Ubuntu 14.04 is way behind official docker repo)

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Update your sources
+# Add the Docker repository key to local keychain
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Make sure the /etc/apt/sources.list.d directory exists!
+mkdir -p /etc/apt/sources.list.d
+
+# Add the Docker repository to your apt sources list.
+sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+
+# Update sources
 apt-get update
 
 # Install. Confirm install.
-apt-get install -y docker.io
+apt-get install -y lxc-docker
+
+# inotify-tools is useful to see changes in Docker 'mounted' VOLUMEs
+apt-get install -y inotify-tools
 
 # Have docker listening to a particular port
 sed -i 's/DOCKER_OPTS=$/DOCKER_OPTS="-H tcp:\/\/0.0.0.0:4243"/' /etc/init.d/docker.io
