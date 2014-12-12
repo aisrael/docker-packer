5df6ebc05adb8b380fd748116894fb1bf1139354
Alistair A. Israel <aisrael@gmail.com>
2014-12-12 20:09:09 +0800

    Added build.sh to template-ize stuff

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VIRTUALBOX_VERSION=$(VBoxManage --version|sed 's/r.*$//g')
+echo "VIRTUALBOX_VERSION: ${VIRTUALBOX_VERSION}"
+
+DOCKER_VERSION=$(docker version 2>/dev/null|head -1|sed 's/^Client version: //g')
+echo "DOCKER_VERSION: ${DOCKER_VERSION}"
+
+sed -e "s/\${VIRTUALBOX_VERSION}/${VIRTUALBOX_VERSION}/g" -e "s/\${DOCKER_VERSION}/${DOCKER_VERSION}/g" < docker_packer.json.template > docker_packer.json
+sed -e "s/\${VIRTUALBOX_VERSION}/${VIRTUALBOX_VERSION}/g" -e "s/\${DOCKER_VERSION}/${DOCKER_VERSION}/g" < Vagrantfile.template > Vagrantfile
