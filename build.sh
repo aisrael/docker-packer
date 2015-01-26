e4361b008f61101bffdfa971a56c580240e93661
Alistair A. Israel <aisrael@gmail.com>
2015-01-26 14:45:57 +0800

    Use `docker --version`

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 VIRTUALBOX_VERSION=$(VBoxManage --version|sed 's/r.*$//g')
 echo "VIRTUALBOX_VERSION: ${VIRTUALBOX_VERSION}"
 
-DOCKER_VERSION=$(docker version 2>/dev/null|head -1|sed 's/^Client version: //g')
+DOCKER_VERSION=$(docker --version 2>/dev/null|sed -n 's/^Docker version \(.*\),.*/\1/p')
 echo "DOCKER_VERSION: ${DOCKER_VERSION}"
 
 sed -e "s/\${VIRTUALBOX_VERSION}/${VIRTUALBOX_VERSION}/g" -e "s/\${DOCKER_VERSION}/${DOCKER_VERSION}/g" < docker_packer.json.template > docker_packer.json
