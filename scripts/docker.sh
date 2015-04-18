87128524db69e932ce8a9e184f556682f72c1660
Alistair A. Israel <aisrael@gmail.com>
2015-04-18 08:52:47 +0800

    Use new Docker installer script for Ubuntu

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Install extra kernel packages
-apt install -y linux-image-extra-$(uname -r)
-
-# Add the Docker repository key to local keychain
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-
-# Make sure the /etc/apt/sources.list.d directory exists!
-mkdir -p /etc/apt/sources.list.d
-
-# Add the Docker repository to your apt sources list.
-sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-
-# Update sources
-apt-get update
-
-# Install. Confirm install.
-apt-get install -y lxc-docker
+# See http://docs.docker.com/installation/ubuntulinux/#installing-docker-on-ubuntu
+bash -c 'wget -qO- https://get.docker.com/ | sh'
 
 # inotify-tools is useful to see changes in Docker 'mounted' VOLUMEs
 apt-get install -y inotify-tools
